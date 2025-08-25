d8654b400630dc0d20b013a7cf9b2be700062395
Pranta Saha <prantasaha907@gmail.com>
2025-08-25 21:47:24 +0600

    Meal Entity class

--- MealManagement/src/main/java/com/pranta/MealManagement/Entity/MealEntity.java
+++ MealManagement/src/main/java/com/pranta/MealManagement/Entity/MealEntity.java
@@ -1,5 +1,38 @@
 package com.pranta.MealManagement.Entity;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "male_entity")
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MealEntity {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Not
+    private Member member;
+    private LocalDate date;
+
+    private MealType mealType;
+
+    private int mealCount = 1;
+    public enum MealType{
+        BREAKFAST, LUNCH, DINNER
+    } 
 }
