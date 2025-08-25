0a29a5c9ed107be8e4c3a268e517194f214f8ece
Pranta Saha <prantasaha907@gmail.com>
2025-08-25 22:26:15 +0600

    Deposite entity class

--- MealManagement/src/main/java/com/pranta/MealManagement/Entity/Deposit.java
+++ MealManagement/src/main/java/com/pranta/MealManagement/Entity/Deposit.java
@@ -1,5 +1,48 @@
 package com.pranta.MealManagement.Entity;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.IdGeneratorType;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "deposits")
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Deposit {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id",nullable = false)
+    private Member member;
+
+    @NotNull(message = "Amount is required")
+    @DecimalMin(value = "0.0",inclusive = false,message = "Amount must be positive")
+    @Column(precision = 10, scale = 2, nullable = false)
+    private BigDecimal amount;
+
+    @Column(name = "deposite_date")
+    private LocalDateTime depositeDate;
+    
+    private String discription;
 }
