8d46c18469c4c192c91ca5094cc820bf78eb4df4
Pranta Saha <prantasaha907@gmail.com>
2025-08-25 21:48:45 +0600

    Meal entity class

--- MealManagement/src/main/java/com/pranta/MealManagement/Entity/MealEntity.java
+++ MealManagement/src/main/java/com/pranta/MealManagement/Entity/MealEntity.java
@@ -2,13 +2,19 @@ package com.pranta.MealManagement.Entity;
 
 import java.time.LocalDate;
 
+import jakarta.annotation.Nullable;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -25,12 +31,18 @@ public class MealEntity {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @Not
+    @JoinColumn(name = "member_id",nullable = false)
     private Member member;
+
+    @NotBlank(message = "Date is required")
+    @Column(name = "date")
     private LocalDate date;
 
+    @Enumerated(EnumType.STRING)
+    @NotBlank(message = "Meal Type is required")
     private MealType mealType;
 
+    @Column(name = "meal_count")
     private int mealCount = 1;
     public enum MealType{
         BREAKFAST, LUNCH, DINNER
