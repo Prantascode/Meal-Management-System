89ec73ceee34127b4b8322ae74ea754ac4d44c90
Pranta Saha <prantasaha907@gmail.com>
2025-08-28 00:51:31 +0600

    Member dto

---
+++ MealManagement/src/main/java/com/pranta/MealManagement/Dtos/MemberDto.java
@@ -0,0 +1,33 @@
+package com.pranta.MealManagement.Dtos;
+
+import java.time.LocalDateTime;
+
+import com.pranta.MealManagement.Entity.Member;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MemberDto {
+    private Long id;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Invalid email formate")
+    private String email;
+
+    @NotBlank(message = "Phone number is required")
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    private Member.Role role;
+
+    private boolean active = true;
+}
