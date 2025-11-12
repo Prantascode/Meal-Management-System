e809b7ddc619c19078d8032307cd23b2261592c7
Pranta Saha <prantasaha907@gmail.com>
2025-11-12 21:41:08 +0600

    find deposit by member and total deposit by member in date range

--- MealManagement/src/main/java/com/pranta/MealManagement/Controller/DepositController.java
+++ MealManagement/src/main/java/com/pranta/MealManagement/Controller/DepositController.java
@@ -1,14 +1,22 @@
 package com.pranta.MealManagement.Controller;
 
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pranta.MealManagement.Dtos.DepositDto;
@@ -31,5 +39,24 @@ public class DepositController {
         }catch (RuntimeException e){
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @GetMapping("/member/{memberId}")
+    public ResponseEntity<List<DepositDto>> findDepositByMember(@PathVariable Long memberId){
+        try{
+            List<DepositDto> deposit = depositService.getDepositByMember(memberId);
+            return ResponseEntity.ok(deposit);
+        }catch(RuntimeException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/total/{memberId}/range")
+    public ResponseEntity<BigDecimal> findTotalDepositByMemberAndDateRange(@PathVariable Long memberId,
+        @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE.TIME)LocalDateTime startDate,
+        @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE.TIME)LocalDateTime endDate){
+
+        BigDecimal totalDeposit = depositService.getDepositByMemberAndDateRange(memberId, startDate, endDate);
+        return ResponseEntity.ok(totalDeposit);
     }
 }
--- MealManagement/src/main/java/com/pranta/MealManagement/Service/DepositService.java
+++ MealManagement/src/main/java/com/pranta/MealManagement/Service/DepositService.java
@@ -2,6 +2,8 @@ package com.pranta.MealManagement.Service;
 
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -29,6 +31,26 @@ public class DepositService {
         Deposit savedDeposit =  depositRepository.save(deposit);
 
         return convertToDto(savedDeposit);
+    }
+
+    public List<DepositDto> getDepositByMember(Long memberId){
+       Member member = memberRepository.findById(memberId)
+            .orElseThrow(() -> new RuntimeException("Member not found"));
+        
+        List<Deposit> deposits = depositRepository.findByMemberOrderByDepositDateDesc(member);
+        
+        return deposits.stream()
+                        .map(this::convertToDto)
+                        .toList();
+    }
+
+    public BigDecimal getDepositByMemberAndDateRange(Long memberId,LocalDateTime startDate,LocalDateTime endDate){
+        Member member = memberRepository.findById(memberId)
+            .orElseThrow(() -> new RuntimeException("Member not found"));
+
+        BigDecimal total = depositRepository.getTotalDepositsByMemberBetweenDates(member, startDate, endDate);
+
+        return total != null ? total : BigDecimal.ZERO;
     }
 
     private DepositDto convertToDto(Deposit deposit){
