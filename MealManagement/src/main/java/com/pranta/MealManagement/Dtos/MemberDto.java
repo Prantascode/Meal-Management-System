3f660db7261c83b6208029e4a6a1ce38f3586c9d
Pranta Saha <prantasaha907@gmail.com>
2026-05-13 22:04:16 +0600

    Updated

--- MealManagement/src/main/java/com/pranta/MealManagement/Dtos/MemberDto.java
+++ MealManagement/src/main/java/com/pranta/MealManagement/Dtos/MemberDto.java
@@ -3,10 +3,7 @@ package com.pranta.MealManagement.Dtos;
 
 import com.pranta.MealManagement.Entity.Member;
 
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
